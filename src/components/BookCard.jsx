a1f3973726e2720bf7f6e18f72f457226b4b26fc
Wassim Triki <wsmtriki@gmail.com>
2022-04-13 16:39:50 +0100

    fix button layout in card on lg

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -22,7 +22,7 @@ const BookCard = ({ book, categoryName }) => {
   return (
     <div className="bg-gray-100 dark:bg-bg-dark-50 dark-mode-transition w-full shadow-sm rounded-2xl  p-3 relative min-h-[300px] grid grid-cols-2  font-main min-w-[300px]">
       <div className="relative ">
-        <div className="absolute h-[200px] w-[135px]  rounded-2xl -top-10 flex justify-center">
+        <div className="  absolute h-[200px] w-[135px] lg:h-[250px] lg:w-[180px] rounded-2xl -top-10 flex justify-center">
           <img
             className="shadow-xl hover:shadow-2xl cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out object-cover h-full rounded-2xl"
             src={info.imageLinks.thumbnail}
@@ -41,14 +41,16 @@ const BookCard = ({ book, categoryName }) => {
         >
           {info.categories}
         </span>
-        <div className="mt-4 flex gap-2  xl:items-center justify-start  flex-col md:flex-row ">
-          <button className="btn bg-green-400  text-white flex-grow p-2 rounded-full hover:bg-green-300 focus:ring-green-300 ">
-            Want to read
-          </button>
-          <button className="btn bg-yellow-400  text-white flex-grow p-2 rounded-full hover:bg-yellow-300 focus:ring-yellow-300 ">
-            Reviews
-          </button>
-          <button className="btn bg-transparent focus:bg-red-400  focus:text-white text-red-400 flex-grow p-2 border-2 border-red-400 rounded-full hover:bg-red-300 focus:ring-red-300">
+        <div className="flex flex-col gap-2">
+          <div className="mt-4 flex gap-2  xl:items-center justify-start  flex-col md:flex-row ">
+            <button className="btn bg-green-400  text-white flex-grow p-2 rounded-full hover:bg-green-300 focus:ring-green-300 ">
+              Want to read
+            </button>
+            <button className="btn bg-yellow-400  text-white flex-grow p-2 rounded-full hover:bg-yellow-300 focus:ring-yellow-300 ">
+              Reviews
+            </button>
+          </div>
+          <button className="btn bg-transparent focus:bg-red-400 hover:text-white  focus:text-white text-red-400 flex-grow p-2 border-2 border-red-400 rounded-full hover:bg-red-400 focus:ring-red-300 ">
             Favourite
           </button>
         </div>
