a4a44d12e7f5d8a43fc470fc3b64fa2293fd2353
Wassim Triki <wsmtriki@gmail.com>
2022-04-17 21:37:31 +0100

    hide footer when not logged in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from '../context/UserContext';
 
 const Footer = () => {
+  const { user } = useContext(UserContext);
   return (
-    <footer className="h-[50px] w-full border-t-2 border-gray-200 dark:border-bg-dark-50 p-10 text-bg-dark dark:bg-bg-dark dark:text-white dark-mode-transition flex items-center justify-center font-main">
+    <footer
+      className={`h-[50px] w-full border-t-2 border-gray-200 dark:border-bg-dark-50 p-10 text-bg-dark dark:bg-bg-dark dark:text-white dark-mode-transition flex items-center justify-center font-main ${
+        user ? 'flex' : 'hidden'
+      }`}
+    >
       <p>&copy; Wassim Triki 2022</p>
     </footer>
   );
